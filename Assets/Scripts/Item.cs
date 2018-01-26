375e5476348e2d274020d95757187d367e2211f4
Arnaud Briche <arnaudbriche1994@gmail.com>
2018-01-26 23:51:01 +0100

    Created a few items

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using ScriptableFramework.Variables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variables/Item")]
+public class Item : ScriptableObject
+{
+    public string Name;
+    public Sprite Image;
+    public string Description;
+    public float BasePrice;
+    public float PriceIncrease;
+    public string StupidQuote;
+    public int NbItemsSold;
+
+    public void NewPrice()
+    {
+        BasePrice *= Mathf.Pow(PriceIncrease, NbItemsSold);
+        NbItemsSold += 1;
+    }
+}
