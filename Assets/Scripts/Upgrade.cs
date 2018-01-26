52f28f4058fd5a40349c92463ba853d4803bcbbe
ThmsMsqrt <thomas.masquart@absolute.de>
2018-01-26 23:47:38 +0100

    Add the upgrade scriptable object

---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variables/Upgrade")]
+public class Upgrade : ScriptableObject
+{
+    public bool IsUnlocked;
+    public string Name;
+    public string Description;
+    public string FluffText;
+    public Sprite Sprite;
+    public float AlphaColor;
+    public float Cost;
+}
