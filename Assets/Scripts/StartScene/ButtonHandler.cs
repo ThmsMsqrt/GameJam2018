6d9d0fc9bc88ddd85b63cb7f28c5e2dbb9ea9f50
Arnaud Briche <arnaudbriche1994@gmail.com>
2018-01-26 21:50:42 +0100

    Basic layout + basic start screen

---
+++ Assets/Scripts/StartScene/ButtonHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour {
+
+    public Text NameOfGame;
+
+    /// <summary>
+    /// Method to Start the game
+    /// </summary>
+    public void StartApp()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    /// <summary>
+    /// Method to Stop the game
+    /// </summary>
+	public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Method to load a previous game
+    /// </summary>
+    public void LoadGame()
+    {
+        NameOfGame.text = "WUUUUUUUUUUUUUUUUUUUUUUUUUT";
+        //TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+}
