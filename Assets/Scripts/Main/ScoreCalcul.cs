890bcfed7060443e327edc1b9d004937356a0276
Arnaud Briche <arnaudbriche1994@gmail.com>
2018-01-26 22:45:47 +0100

    Added basic click, displayer, DPS and DPC

---
+++ Assets/Scripts/Main/ScoreCalcul.cs
@@ -0,0 +1,37 @@
+﻿using ScriptableFramework.Variables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalcul : MonoBehaviour {
+
+    float basicClickValue = 1;
+    float timing = 0f;
+
+    public FloatVariable Score;
+    public FloatVariable DPS;
+    public FloatVariable DPC;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Init score value
+        Score.SetValue(0f);
+        DPS.SetValue(0.01f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Score.ApplyChange(DPS.Value);
+    }
+
+    public void CalculateClickScore()
+    {
+        float scoreToApply = basicClickValue;
+
+        //Add factor calculation for the click
+        scoreToApply *= DPC.Value;
+        Score.ApplyChange(scoreToApply);
+    }
+}
