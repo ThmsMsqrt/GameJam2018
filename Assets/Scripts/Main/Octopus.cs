289a3ff31eab43ecf85076b940b53a0469aaee92
Arnaud Briche <arnaudbriche1994@gmail.com>
2018-01-27 15:49:15 +0100

    Added sprite switch with DPS

--- Assets/Scripts/Main/Octopus.cs
+++ Assets/Scripts/Main/Octopus.cs
@@ -7,12 +7,13 @@ namespace Assets.Scripts.Main
     public class Octopus : MonoBehaviour
     {
         public SpriteManager OctopusStates;
-        public FloatReference Score;
+        public FloatReference DPS;
         public AudioManager AudioClips;
         private AudioSource _audio;
 
         private Image ImageOctopus;
         private int OctopusStateStart = 0;
+        private float thresholdEvolve = 100f;
 
         public void Start()
         {
@@ -26,18 +27,22 @@ namespace Assets.Scripts.Main
 
         public void Update()
         {
-            if(Score.Value > 10)
+            if(DPS.Value > thresholdEvolve)
             {
+                Debug.Log("Hello");
                 if (OctopusStateStart < OctopusStates.Sprites.Length)
                 {
+                    Debug.Log("Hello 2");
                     if (ImageOctopus != null)
                     {
-                        ImageOctopus.sprite = OctopusStates.Sprites[OctopusStateStart];
+                        Debug.Log("Hello 3");
                         ++OctopusStateStart;
+                        ImageOctopus.sprite = OctopusStates.Sprites[OctopusStateStart];
                         if (_audio != null)
                         {
                             _audio.PlayOneShot(AudioClips.SquidEvolveSound);
                         }
+                        thresholdEvolve = 200f;
                     }
                 }
             }
