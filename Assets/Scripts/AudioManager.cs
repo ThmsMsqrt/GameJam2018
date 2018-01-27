0afe7d6e66501e38dd26fcbaf7612cd00e380db9
ThmsMsqrt <thomas.masquart@absolute.de>
2018-01-27 01:03:33 +0100

    Add the base for audio management

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Manager/AudioManager")]
+public class AudioManager : ScriptableObject
+{
+    public AudioClip BackgroundMusic;
+    public AudioClip SquidClickSound;
+    public AudioClip UpgradeClickSound;
+    public AudioClip ItemClickSound;
+}
