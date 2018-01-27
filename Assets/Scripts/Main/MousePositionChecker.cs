d5e6b97712b39d336984c832d7a0d280e118682b
Arnaud Briche <arnaudbriche1994@gmail.com>
2018-01-27 02:04:58 +0100

    Added info panel

---
+++ Assets/Scripts/Main/MousePositionChecker.cs
@@ -0,0 +1,26 @@
+﻿using ScriptableFramework.Variables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MousePositionChecker : MonoBehaviour
+{
+    public GameObjectReference InfoPanel;
+    public ScriptableObject ObjectBelow;
+
+    Ray ray;
+    RaycastHit hit;
+
+    public void DisplayInfoPanel()
+    {
+        Debug.Log(gameObject);
+        InfoPanel.Value.SetActive(true);
+    }
+
+    public void HideInfoPanel()
+    {
+        Debug.Log(gameObject);
+        InfoPanel.Value.SetActive(false);
+    }
+}
